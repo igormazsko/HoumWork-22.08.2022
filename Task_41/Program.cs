91db88a3d9a440b4a822d12f484351838f5d5510
Igor Matsko <igormazsko@mail.ru>
2022-08-27 22:50:38 +0300

    add Task 41

---
+++ Task_41/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры М чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь. 
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223 -> 4
+using static System.Console;
+using System.Linq;
+Clear();
+WriteLine("Введите несколько чисел (через пробел): ");
+int [] array = ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(x=>int.Parse(x)).ToArray();
+WriteLine($"Чисел больше ноля - {String.Join(",",QuantPositive(array))}");
+
+
+
+int QuantPositive(int[]arr)
+{
+    int result = 0;
+    for (int i =0; i < arr.Length; i++)
+    {
+        if(arr[i]>0)
+        {
+            result++;
+        }
+    }
+    return result;
+}
