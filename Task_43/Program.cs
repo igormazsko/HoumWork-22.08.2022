ec64f97cffb784d6b5535aab4de344235b26a73e
Igor Matsko <igormazsko@mail.ru>
2022-08-27 23:25:37 +0300

    add Task 43

---
+++ Task_43/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y=k1*x+b1, y=k2*x+b2; значения b1, k1, 
+// b2 и k2 задаются пользователем.
+// y=k1*x+b1, y=k2*x+b2
+// k1*x+b1=k2*x+b2
+// k1*x-k2*x=b2-b1
+// x*(k1-k2)=b2-b1
+// x=(b2-b1)/(k1-k2)
+using static System.Console;
+Clear();
+Write("Введите коордитнату k1 - ");
+double k1 = Convert.ToDouble(ReadLine());
+Write("Введите коордитнату b1 - ");
+double b1 = Convert.ToDouble(ReadLine());
+Write("Введите коордитнату k2 - ");
+double k2 = Convert.ToDouble(ReadLine());
+Write("Введите коордитнату b2 - ");
+double b2 = Convert.ToDouble(ReadLine());
+double x=(b2-b1)/(k1-k2);
+double y=k1*x+b1;
+WriteLine ($"Точка пересечения прямых: ({x};{y})");
